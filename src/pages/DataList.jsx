70c52fb563d9ceb06fa10c7665c0c27a746aaaa8
drianfth <andrianfathulloh05@gmail.com>
2023-05-13 13:28:48 +0700

    tes

--- src/pages/DataList.jsx
+++ src/pages/DataList.jsx
@@ -7,24 +7,24 @@ import { useMutation, useQuery } from "react-query";
 const DataList = () => {
   const location = useLocation();
   // console.log(location.state.id);
-  const datalist = useQuery("datalist", () =>
-    getAllDetailData(location.state.id)
-  );
-  console.log(pendudukMiskin);
-  const columns = useMemo(
-    () => [
-      datalist.data?.var.map((variable, index) => ({
-        Header: variable.name.toLowerCase().replace(/\s/g, ""),
-        accessor: variable.name.toLowerCase().replace(/\s/g, ""),
-      })),
-    ],
-    []
-  );
-  // let dataTable = {}
-  // bulog.data.map((value,index) => ({
+  // const datalist = useQuery("datalist", () =>
+  //   getAllDetailData(location.state.id)
+  // );
+  // console.log(pendudukMiskin);
+  // const columns = useMemo(
+  //   () => [
+  //     datalist.data?.var.map((variable, index) => ({
+  //       Header: variable.name.toLowerCase().replace(/\s/g, ""),
+  //       accessor: variable.name.toLowerCase().replace(/\s/g, ""),
+  //     })),
+  //   ],
+  //   []
+  // );
+  // // let dataTable = {}
+  // // bulog.data.map((value,index) => ({
 
-  // }))
-  const dataTable = useMemo(() => datalist.data?.data, []);
+  // // }))
+  // const dataTable = useMemo(() => datalist.data?.data, []);
 
   // console.log(columns[0]);
   return (
@@ -33,7 +33,7 @@ const DataList = () => {
         DataList
         <div className="w-full h-0.5 bg-gray-100 mt-3"></div>
       </div>
-      <DataTable columns={columns[0]} data={dataTable} />
+      {/* <DataTable columns={columns[0]} data={dataTable} /> */}
     </div>
   );
 };
