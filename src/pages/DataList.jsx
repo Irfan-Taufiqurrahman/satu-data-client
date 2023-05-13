67c8d5675997787af9cec4c186b2c963e6a7a78f
Irfan-Taufiqurrahman <irfan.rachman72@gmail.com>
2023-05-13 13:26:50 +0700

    Testing Code

--- src/pages/DataList.jsx
+++ src/pages/DataList.jsx
@@ -3,6 +3,8 @@ import { useLocation } from "react-router-dom";
 import { pendudukMiskin, bulog } from "../data/datalist";
 import DataTable from "../components/DataTable";
 import { useMutation, useQuery } from "react-query";
+import { getAllDetailData } from "../api/Dataset";
+import Loading from '../components/Loading';
 
 const DataList = () => {
   const location = useLocation();
@@ -10,30 +12,28 @@ const DataList = () => {
   const datalist = useQuery("datalist", () =>
     getAllDetailData(location.state.id)
   );
-  console.log(pendudukMiskin);
   const columns = useMemo(
     () => [
-      datalist.data?.var.map((variable, index) => ({
+      datalist.data?.var?.map((variable, index) => ({
         Header: variable.name.toLowerCase().replace(/\s/g, ""),
         accessor: variable.name.toLowerCase().replace(/\s/g, ""),
       })),
     ],
-    []
-  );
-  // let dataTable = {}
-  // bulog.data.map((value,index) => ({
+    [datalist.data]
+    ); 
+    
+    const dataTable = useMemo(() => datalist.data?.data, [datalist.data]);
+    // console.log(dataTable);
 
-  // }))
-  const dataTable = useMemo(() => datalist.data?.data, []);
-
-  // console.log(columns[0]);
+  // console.log(columns);
   return (
     <div>
       <div className="text-center font-bold pb-8 text-xl text-gray-800">
         DataList
         <div className="w-full h-0.5 bg-gray-100 mt-3"></div>
       </div>
-      <DataTable columns={columns[0]} data={dataTable} />
+      {datalist.isLoading ? <Loading /> : <DataTable columns={columns[0]} data={dataTable} />}
+      
     </div>
   );
 };
